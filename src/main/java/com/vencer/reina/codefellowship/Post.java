e90ddd7b01ea31b6ef901391644104833692e36d
river-ceanne <snvencer@gmail.com>
2019-06-19 10:34:38 -0700

    Post class made with @manytoOne rel to AppUser

--- src/main/java/com/vencer/reina/codefellowship/Post.java
+++ src/main/java/com/vencer/reina/codefellowship/Post.java
@@ -1,6 +1,7 @@
 package com.vencer.reina.codefellowship;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 public class Post {
@@ -10,5 +11,45 @@ public class Post {
     // A logged-in user should be able to create a Post, and a post should belong to the user that created it.
     // hint: this is a relationship between two pieces of data
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
 
+    String body;
+    Timestamp createdAt;
+
+    @ManyToOne
+    AppUser appUser;
+
+    public Post(){}
+
+    public Post(String body, Timestamp createdAt, AppUser appUser) {
+        this.body = body;
+        this.createdAt = createdAt;
+        this.appUser = appUser;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
 }
