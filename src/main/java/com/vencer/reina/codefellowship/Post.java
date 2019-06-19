ba37d2f36fc39d204f55e692f7c7f8f49cebee88
river-ceanne <snvencer@gmail.com>
2019-06-19 10:29:41 -0700

    added Post.java and style.css files

---
+++ src/main/java/com/vencer/reina/codefellowship/Post.java
@@ -0,0 +1,14 @@
+package com.vencer.reina.codefellowship;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Post {
+
+    // Add a Post entity to your app.
+    // A Post has a body and a createdAt timestamp.
+    // A logged-in user should be able to create a Post, and a post should belong to the user that created it.
+    // hint: this is a relationship between two pieces of data
+
+
+}
